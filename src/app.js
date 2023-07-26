e45571f4ca2482bfc7909133dc6853757548dba8
aguramirez <agustinramirez58@gmail.com>
2023-07-26 11:04:14 -0300

    cambiando port para deploy

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3030, () => console.log('Servidor abierto'));
+// app.listen(3030, () => console.log('Servidor abierto'));
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/index.html"))
